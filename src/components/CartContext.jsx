043b9ffda48bb4f8bff40e361acad76d63e6715e
Idir FOUQUET <fouquetidir@gmail.com>
2024-04-25 20:52:42 +0200

    AddToCart / 1 quantity = 1 object

--- src/components/CartContext.jsx
+++ src/components/CartContext.jsx
@@ -1,42 +1,34 @@
-import React, { createContext, useContext, useState, useEffect } from 'react';
+import React, { createContext, useContext, useState, useEffect } from "react";
 
-//We create a "cart context" to use it everywhere in the app into children components 
+//We create a "cart context" to use it everywhere in the app into children components
 const CartContext = createContext();
 
 export function CartProvider({ children }) {
-  const [cart, setCart] = useState(()=> {
-    const savedCart = localStorage.getItem('cart'); //We store the cart inside the local strorage to avoid to lose it
+  const [cart, setCart] = useState(() => {
+    const savedCart = localStorage.getItem("cart"); //We store the cart inside the local strorage to avoid to lose it
     return savedCart ? JSON.parse(savedCart) : [];
   });
 
   useEffect(() => {
-    // We store the cart everytime it changes 
-    localStorage.setItem('cart', JSON.stringify(cart));
+    // We store the cart everytime it changes
+    localStorage.setItem("cart", JSON.stringify(cart));
   }, [cart]);
 
-  
-
   //The function to allow you to add something in your cart
-  const addToCart = (item, quantity) => { //take the item and the quantity added 
+  const addToCart = (item, quantity) => {
     setCart((prevCart) => {
-        const existingItemIndex = prevCart.findIndex(cartItem => cartItem.item_id === item.item_id); //Check if the item already exists
-        if (existingItemIndex >= 0) {
-          //If it exists we only add the quantity
-          const updatedCart = [...prevCart];
-          updatedCart[existingItemIndex] = {
-            ...updatedCart[existingItemIndex],
-            quantityProduct: updatedCart[existingItemIndex].quantityProduct + quantity
-          };
-          return updatedCart;
-        } else {
-          //If not we add it completely to the cart 
-          return [...prevCart, { ...item, quantityProduct: quantity }];
-        }
-      });
-      console.log(cart)
+      const newCart = [...prevCart]; 
+      for (let i = 0; i < quantity; i++) { //Add an object for each quantity
+        newCart.push({ ...item }); 
+      }
+      return newCart;
+    });
   };
 
-  const cartCount = cart.reduce((count, item) => count + item.quantityProduct, 0);
+  const cartCount = cart.reduce(
+    (count, item) => count + item.quantityProduct,
+    0
+  );
 
   return (
     <CartContext.Provider value={{ cart, addToCart, cartCount }}>
