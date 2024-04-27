174ea9223927eb2183a504ecb1c543291c606024
Idir FOUQUET <fouquetidir@gmail.com>
2024-04-27 16:32:01 +0200

    Fix Maintitle responsivenss

--- src/components/MainTitle.jsx
+++ src/components/MainTitle.jsx
@@ -1,30 +1,34 @@
-import React from 'react';
-import Typography from '@mui/material/Typography';
+import React from "react";
+import Typography from "@mui/material/Typography";
 import useMediaQuery from "@mui/material/useMediaQuery";
 import { useTheme } from "@mui/material/styles";
 
 const MainTitle = ({ children }) => {
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
-  
+
   return (
     <Typography
+      className="main-title"
       sx={{
         fontSize: {
           sm: "2.5rem",
           md: "3rem",
         },
         flexGrow: 1,
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontFamily: 'PT Sans Caption, sans-serif',
-        fontWeight: 'bold',
-        fontSize: '4rem',
-        color: 'black',
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        fontFamily: "PT Sans Caption, sans-serif",
+        fontWeight: "bold",
+        fontSize: "4rem",
+        color: "black",
         mt: 4,
         mb: 4,
         opacity: 0.1,
+        "@media screen and (max-width: 768px)": {
+          display: "none",
+        },
       }}
     >
       {children}
